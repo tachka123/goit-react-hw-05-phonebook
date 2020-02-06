8eddfeede8d8dd9268ce63de8376031cfb4834ed
Sasha Savchuk <Sasha>
2020-02-06 02:14:20 +0200

    Updates

---
+++ precache-manifest.572f8101ab8fca5801e6a0bcf67d4579.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6e291e9103e0193523ff502d6087804",
+    "url": "/goit-react-hw-05-phonebook/index.html"
+  },
+  {
+    "revision": "d98db54e86eb7b83e5ac",
+    "url": "/goit-react-hw-05-phonebook/static/css/main.22435a18.chunk.css"
+  },
+  {
+    "revision": "e6bd16ef9dd80905c57f",
+    "url": "/goit-react-hw-05-phonebook/static/js/2.7dc9e00b.chunk.js"
+  },
+  {
+    "revision": "d98db54e86eb7b83e5ac",
+    "url": "/goit-react-hw-05-phonebook/static/js/main.d8261838.chunk.js"
+  },
+  {
+    "revision": "8041125f0135d131b793",
+    "url": "/goit-react-hw-05-phonebook/static/js/runtime-main.88c2bd4c.js"
+  }
+]);
